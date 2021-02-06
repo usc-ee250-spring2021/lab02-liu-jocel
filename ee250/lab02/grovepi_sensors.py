e45f54b3cbc988bb8234b64511c89d7d7bc07f93
Jocelyn Liu <liujocel@usc.edu>
2021-02-05 23:33:22 -0500

    Changes to be committed: 	modified:   grovepi_sensors.py

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -1,3 +1,5 @@
+
+
 """ EE 250L Lab 02: GrovePi Sensors
 
 List team members here.
@@ -16,6 +18,7 @@ performance. Because of this, you will not find this in the default directories.
 """
 import sys
 import time
+from grove_rgb_lcd import * 
 # By appending the folder of all the GrovePi libraries to the system path here,
 # we are successfully `import grovepi`
 sys.path.append('../../Software/Python/')
@@ -28,11 +31,29 @@ import grovepi
 is, if you run `python3 grovepi_sensors.py` in terminal, this if-statement will 
 be true"""
 if __name__ == '__main__':
+
+    setRGB(0,255,0)
     PORT = 4    # D4
+    POT = 0	# A0
 
     while True:
         #So we do not poll the sensors too quickly which may introduce noise,
         #sleep for a reasonable time of 200ms between each iteration.
         time.sleep(0.2)
+	threshold = grovepi.analogRead(POT)
+	dist = grovepi.ultrasonicRead(PORT)
 
-        print(grovepi.ultrasonicRead(PORT))
+	if dist <= threshold:
+	   l1 = str(threshold)+" cm OBJ PRES"
+	   mask = " "
+	   while len(l1+mask) < 16:
+		mask += " "
+	   setText_norefresh(l1+mask+"\n"+str(dist)+" cm")
+	   setRGB(255,0,0)
+	else:
+	   l1 = str(threshold)+" cm"
+	   mask = " "
+	   while len(l1+mask) < 16:
+		mask += " "
+	   setText_norefresh(l1+mask+"\n"+str(dist)+" cm")
+	   setRGB(0,255,0)
