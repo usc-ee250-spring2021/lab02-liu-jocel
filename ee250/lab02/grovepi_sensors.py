687373199f1910334601cc77586860c48a2593eb
Jocelyn Liu <liujocel@usc.edu>
2021-02-07 11:39:35 -0800

    2nd one

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -1,6 +1,8 @@
+
+
 """ EE 250L Lab 02: GrovePi Sensors
 
-List team members here.
+Jocelyn Liu
 
 Insert Github repository link here.
 """
@@ -16,6 +18,7 @@ performance. Because of this, you will not find this in the default directories.
 """
 import sys
 import time
+from grove_rgb_lcd import * 
 # By appending the folder of all the GrovePi libraries to the system path here,
 # we are successfully `import grovepi`
 sys.path.append('../../Software/Python/')
@@ -28,11 +31,30 @@ import grovepi
 is, if you run `python3 grovepi_sensors.py` in terminal, this if-statement will 
 be true"""
 if __name__ == '__main__':
+
+    setRGB(0,255,0)
     PORT = 4    # D4
+    POT = 0	# A0
 
     while True:
         #So we do not poll the sensors too quickly which may introduce noise,
         #sleep for a reasonable time of 200ms between each iteration.
         time.sleep(0.2)
-
-        print(grovepi.ultrasonicRead(PORT))
+	threshold = grovepi.analogRead(POT)
+	dist = grovepi.ultrasonicRead(PORT)
+	#Case when distance is within threshold
+	if dist <= threshold:
+	   l1 = str(threshold)+" cm OBJ PRES"
+	   mask = " "
+	   while len(l1+mask) < 16: #Mask til end of line 1 to cover up OBJ PRES if digits change
+		mask += " "
+	   setText_norefresh(l1+mask+"\n"+str(dist)+" cm")
+	   setRGB(255,0,0)
+	#Case when distance is outside threshold
+	else:
+	   l1 = str(threshold)+" cm"
+	   mask = " "
+	   while len(l1+mask) < 16: #Mask line 1 to cover up OBJ PRES if state or digits change
+		mask += " "
+	   setText_norefresh(l1+mask+"\n"+str(dist)+" cm")
+	   setRGB(0,255,0)
